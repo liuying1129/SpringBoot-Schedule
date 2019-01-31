d291ab920deb008bff322f9dd4d7a41ab9c981a7
liuying <liuying@baibu.la>
2019-01-31 22:17:40 +0800

    目录初始化

---
+++ src/main/java/com/yklis/YkSchedule/YkScheduleApplication.java
@@ -0,0 +1,14 @@
+package com.yklis.YkSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YkScheduleApplication.class, args);
+	}
+
+}
+
