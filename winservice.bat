61d31f29117c344f9a4bbb7e07c7d00175182ce5
liuying <liuying@baibu.la>
2019-04-05 21:33:15 +0800

    配置为windows服务

---
+++ winservice.bat
@@ -0,0 +1,6 @@
+D:\source_reference\SpringBoot-Schedule\WinSW.NET4.exe install
+net start yklisschedule
+::net stop yklisschedule
+::D:\source_reference\SpringBoot-Schedule\WinSW.NET4.exe uninstall
+
+pause
