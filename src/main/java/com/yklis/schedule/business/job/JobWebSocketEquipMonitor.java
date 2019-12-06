d36daf42d1071e52e0eefeb0e3ead938a11157f2
liuying <liuying@baibu.la>
2019-12-06 22:22:51 +0800

    当天、当月样本数量

--- src/main/java/com/yklis/schedule/business/job/JobWebSocketEquipMonitor.java
+++ src/main/java/com/yklis/schedule/business/job/JobWebSocketEquipMonitor.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.alibaba.fastjson.JSON;
 import com.yklis.schedule.misc.WebSocketEquipMonitor;
 import com.yklis.schedule.util.SpringUtils;
 
@@ -28,12 +29,10 @@ public class JobWebSocketEquipMonitor implements Command {
     //PropertyConfigurator.configure("log4jj.properties");
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     
-    //类全局使用(递增),故使用static
-    //private static int unid;
-
 	@Override
 	public void execute(Map<String, Object> map) {
 		
+		if(null == WebSocketEquipMonitor.wsSet) return;//表示没有socket客户端连接
 		if(WebSocketEquipMonitor.wsSet.size()<=0) return;//表示没有socket客户端连接
 		
 		JdbcTemplate jdbcTemplate = SpringUtils.getBean(JdbcTemplate.class);
@@ -41,14 +40,14 @@ public class JobWebSocketEquipMonitor implements Command {
         List<Map<String, Object>> list = null;
         try{
 
-        	list = jdbcTemplate.queryForList("SELECT Unid,Type,Model,Remark,Supplier,Brand,ManuFacturer,Create_Date_Time FROM EquipManage WITH(NOLOCK) ");
+        	list = jdbcTemplate.queryForList("SELECT * FROM EquipManage WITH(NOLOCK) ");
         }catch(Exception e){
             logger.error("jdbcTemplate.queryForList失败:"+e.toString());
         }
         
         for(Map<String, Object> map1 : list) {
         	
-			String equipUnid = map1.get("unid").toString();
+			String equipUnid = map1.get("Unid").toString();
 			
 			//获取指定设备最后检验结果所对应的pkunid
 	        List<Map<String, Object>> list3 = null;
@@ -72,7 +71,7 @@ public class JobWebSocketEquipMonitor implements Command {
         	if(Integer.parseInt(pkunid)>0) {        		
 	            try{
 	
-	            	list2 = jdbcTemplate.queryForList("select patientname,check_date from "+("1".equals(ifCompleted)?"chk_con_bak":"chk_con")+" WITH(NOLOCK) where unid="+pkunid);
+	            	list2 = jdbcTemplate.queryForList("select patientname,CONVERT(CHAR(19),check_date,121) AS check_date from "+("1".equals(ifCompleted)?"chk_con_bak":"chk_con")+" WITH(NOLOCK) where unid="+pkunid);
 	            }catch(Exception e){
 	                logger.error("jdbcTemplate.queryForList失败:"+e.toString());
 	            }
@@ -80,28 +79,57 @@ public class JobWebSocketEquipMonitor implements Command {
         	
             String patientname = null;
             String check_date = null;
-	        for(Map<String, Object> map2 : list2) {
-	        	
-	        	patientname = map2.get("patientname").toString();
-	        	check_date = map2.get("check_date").toString();	        	
-	        }
-	        
+            
+            if(null!=list2) {
+            	
+		        for(Map<String, Object> map2 : list2) {
+		        	
+		        	patientname = null==map2.get("patientname") ? null : map2.get("patientname").toString();
+		        	check_date = null==map2.get("check_date") ? null : map2.get("check_date").toString();
+		        }
+            }
+            
+	        //指定设备当天的样本数量
+	        int todayNum = 0;
+        	try{
+        		//sql要求：
+        		//1、有且仅有一条记录
+        		//2、有且仅有一个字段
+        		//3、字段在DB中的类型不限
+        		todayNum = jdbcTemplate.queryForObject("select count(distinct pkunid) from view_chk_valu_All c WITH(NOLOCK),view_Chk_Con_All z WITH(NOLOCK) where z.unid=c.pkunid and CONVERT(CHAR(10),z.check_date,121)=CONVERT(CHAR(10),getdate(),121) and c.EquipUnid="+equipUnid,int.class);
+        	}catch(Exception e){
+                logger.error("jdbcTemplate.queryForObject失败:"+e.toString());
+        	}
+        	
+	        //指定设备当月的样本数量
+	        int thisMonthNum = 0;
+        	try{
+        		//sql要求：
+        		//1、有且仅有一条记录
+        		//2、有且仅有一个字段
+        		//3、字段在DB中的类型不限
+        		thisMonthNum = jdbcTemplate.queryForObject("select count(distinct pkunid) from view_chk_valu_All c WITH(NOLOCK),view_Chk_Con_All z WITH(NOLOCK) where z.unid=c.pkunid and z.check_date>=DATEADD(MONTH,DATEDIFF(MONTH,0,getdate()),0) and z.check_date<=dateadd(ms,-3,DATEADD(mm,DATEDIFF(m,0,getdate())+1,0)) and c.EquipUnid="+equipUnid,int.class);
+        	}catch(Exception e){
+                logger.error("jdbcTemplate.queryForObject失败:"+e.toString());
+        	}
+                        	        
+	        //将病人信息转换为待发送的map
             Map<String, Object> mapSend = new HashMap<>();
             mapSend.put("equipUnid", equipUnid);
-            mapSend.put("patientname", patientname);
-            mapSend.put("check_date", check_date);
-            
-            //return JSON.toJSONString(map);
-			
+            mapSend.put("patientname", null==patientname||"".equals(patientname)?"未知姓名":patientname);
+            mapSend.put("check_date", null==check_date||"".equals(check_date)?"未知时间":check_date);
+            mapSend.put("todayNum", todayNum);
+            mapSend.put("thisMonthNum", thisMonthNum);
+                		
 	        //发送信息
 	        for (WebSocketEquipMonitor wsItem : WebSocketEquipMonitor.wsSet) {
 	            try {
 	           	
-	            	wsItem.getSession().getBasicRemote().sendText(equipUnid + patientname+" "+check_date);
+	            	wsItem.getSession().getBasicRemote().sendText(JSON.toJSONString(mapSend));
 	            } catch (IOException e) {
 	            	logger.error("设备监控WebSocket sendText错误");
 	            }
-	        }			
+	        }
         }
 	}
 }
