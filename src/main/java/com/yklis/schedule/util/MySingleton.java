27734d7087a7e368329c636e45bc61102b4beed2
liuying <liuying@baibu.la>
2020-05-31 16:41:49 +0800

    1111111111111

--- src/main/java/com/yklis/schedule/util/MySingleton.java
+++ src/main/java/com/yklis/schedule/util/MySingleton.java
@@ -10,23 +10,26 @@ import org.quartz.Scheduler;
  */
 public class MySingleton {
 	
-    //JAVA规定，如果类中没有定义任何构造函数，JVM自动为其生成一个默认的构造函数
-    //故可不需要手动写下面的构造函数
-    //public JobSPH2CJ(){       
-    //}
-	
-	//构造函数为private,这样该类就不会被外部实例化
-	private MySingleton() {}  
+	//类中成员变量定义的顺序决定了它们初始化的顺序.即使成员变量定义散布在方法定义之间,它们仍会在任何方法(包括构造器)被调用之前得到初始化
 	
 	//自行实例化
 	//静态数据成员只会被初始化一次
+	//初次创建MySingleton对象或初次使用mySingleton时,mySingleton才会初始化
 	private static final MySingleton mySingleton = new MySingleton();
 	
-	private Scheduler scheduler;//单例类的成员变量
+	private Scheduler scheduler;//单例类的成员变量	
+
+	//JAVA规定，如果类中没有定义任何构造函数，JVM自动为其生成一个默认的构造函数
+    //故可不需要手动写下面的构造函数
+    //public MySingleton(){
+    //}
 	
+	//构造函数为private,这样该类就不会被外部实例化
+	private MySingleton() {}
+		
     //静态工厂方法 
     public static MySingleton getInstance() {  
-        return mySingleton;  
+        return mySingleton;
     }
     
 	public Scheduler getScheduler() {
@@ -36,6 +39,4 @@ public class MySingleton {
 	public void setScheduler(Scheduler scheduler) {
 		this.scheduler = scheduler;
 	}
-
-
 }
